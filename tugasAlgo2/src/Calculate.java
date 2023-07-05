2d9f76310b81f33d5c965262f0c660164ebe0b1d
gmc bjorjka <117149089+gmcbjorjka@users.noreply.github.com>
2023-07-05 09:55:25 +0700

    Add files via upload

---
+++ tugasAlgo2/src/Calculate.java
@@ -0,0 +1,30 @@
+public class Calculate {
+    private int angka1;
+    private int angka2;
+
+    public int getAngka1() {
+        return angka1;
+    }
+
+    public void setAngka1(int angka1) {
+        this.angka1 = angka1;
+    }
+
+    public int getAngka2() {
+        return angka2;
+    }
+
+    public void setAngka2(int angka2) {
+        this.angka2 = angka2;
+    }
+
+    public void addAllNumbers() {
+        int total = angka1 + angka2;
+        System.out.println("Total penjumlahan: " + total);
+    }
+
+    public void multiplyNumbers() {
+        int result = angka1 * angka2;
+        System.out.println("Perkalian: " + result);
+    }
+}
---
+++ tugasAlgo2/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculate calculate = new Calculate();
+        calculate.setAngka1(8);
+        calculate.setAngka2(9);
+        calculate.addAllNumbers();
+        calculate.multiplyNumbers();
+    }
+}
